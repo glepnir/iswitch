25a76f887d90a740f0eda31e1377ac2b79ae8e5c
glepnir <glephunter@gmail.com>
2024-10-21 22:03:03 +0800

    support read config file with hotreload support

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,18 @@ use cocoa::base::{id, nil};
 use cocoa::foundation::NSString;
 use core_foundation::array::{CFArray, CFArrayRef};
 use core_foundation::base::{Boolean, CFTypeRef, TCFType, TCFTypeRef};
-use core_foundation::runloop::CFRunLoopRun;
+use core_foundation::runloop::CFRunLoopRunInMode;
 use core_foundation::string::{CFString, CFStringRef};
+use notify::{Config, Event, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
 use objc::{class, msg_send, sel, sel_impl};
+use serde::Deserialize;
 use std::os::raw::c_void;
+use std::path::{Path, PathBuf};
 use std::ptr;
+use std::sync::mpsc::{channel, Sender};
+use std::sync::{Arc, RwLock};
+use std::thread;
+use std::{collections::HashMap, fs};
 
 #[link(name = "Carbon", kind = "framework")]
 extern "C" {
@@ -21,6 +28,70 @@ extern "C" {
 }
 
 const K_TIS_PROPERTY_INPUT_SOURCE_ID: &str = "TISPropertyInputSourceID";
+const K_CF_RUN_LOOP_DEFAULT_MODE: &str = "kCFRunLoopDefaultMode";
+
+#[derive(Debug, Deserialize)]
+struct AppConfig {
+    app: HashMap<String, String>,
+}
+
+fn get_config_path() -> String {
+    let config_path = if let Ok(config_dir) = std::env::var("XDG_CONFIG_HOME") {
+        PathBuf::from(config_dir)
+    } else if let Ok(home_dir) = std::env::var("HOME") {
+        let mut path = PathBuf::from(home_dir);
+        path.push(".config");
+        path
+    } else {
+        panic!("Neither XDG_CONFIG_HOME nor HOME environment variables are set");
+    };
+
+    let mut final_path = config_path;
+    final_path.push("iswitch");
+    final_path.push("config.toml");
+
+    final_path.to_string_lossy().to_string()
+}
+
+fn ensure_then_load(config_path: &str) -> AppConfig {
+    let path = Path::new(config_path);
+    println!("Config path: {}, exist: {}", config_path, path.exists());
+
+    if !path.exists() {
+        if let Some(parent) = path.parent() {
+            fs::create_dir_all(parent).expect("Failed to create config directory");
+        }
+
+        let default_config = r#"
+[app]
+Alacritty = "com.apple.keylayout.ABC"
+WeChat = "com.apple.inputmethod.SCIM.ITABC"
+QQ = "com.apple.inputmethod.SCIM.ITABC"
+"#;
+
+        fs::write(path, default_config.trim()).expect("Failed to write default config file");
+        println!("Created default config file at: {}", config_path);
+    }
+
+    match fs::read_to_string(config_path) {
+        Ok(config_content) => {
+            println!("Config content read: {}", config_content);
+
+            match toml::from_str::<AppConfig>(&config_content) {
+                Ok(parsed_config) => parsed_config,
+                Err(e) => {
+                    eprintln!("Failed to parse config file: {}", e);
+                    eprintln!("Config content: {}", config_content);
+                    std::process::exit(1);
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("Unable to read config file: {}", e);
+            std::process::exit(1);
+        }
+    }
+}
 
 fn get_current_input_source() -> String {
     unsafe {
@@ -74,83 +145,69 @@ fn switch_input_source(input_source_id: &str) {
     }
 }
 
-fn is_terminal(app_name: &str) -> bool {
-    let supported_terminals = [
-        "Alacritty",
-        "Kitty",
-        "Terminal",
-        "iTerm",
-        "Ghosttty",
-        "foot",
-    ];
-    supported_terminals
-        .iter()
-        .any(|&term| app_name.contains(term))
-}
+fn watch_config_for_changes(config_path: String, tx: Sender<()>) {
+    thread::spawn(move || {
+        let mut watcher = RecommendedWatcher::new(
+            move |res: notify::Result<Event>| {
+                if let Ok(event) = res {
+                    if matches!(event.kind, EventKind::Modify(_)) {
+                        println!("Config file changed, reloading...");
+                        tx.send(()).unwrap();
+                    }
+                }
+            },
+            Config::default(),
+        )
+        .expect("Failed to create watcher");
 
-fn is_chat_app(app_name: &str) -> bool {
-    let chat_apps = ["QQ", "WeChat"];
-    chat_apps.iter().any(|&app| app_name.contains(app))
-}
+        watcher
+            .watch(Path::new(&config_path), RecursiveMode::NonRecursive)
+            .expect("Failed to watch config file");
 
-fn is_iswitch_running() -> bool {
-    let output = std::process::Command::new("pgrep")
-        .arg("iswitch")
-        .output()
-        .expect("Failed to execute pgrep");
-
-    !output.stdout.is_empty()
+        loop {
+            std::thread::park();
+        }
+    });
 }
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let config_path = get_config_path();
+    let config = Arc::new(RwLock::new(ensure_then_load(&config_path)));
 
-    if args.len() > 1 && args[1] == "--check-and-switch" {
-        let current_input_source = get_current_input_source();
-        if current_input_source != "com.apple.keylayout.ABC" {
-            switch_input_source("com.apple.keylayout.ABC");
-        }
-        if is_iswitch_running() {
-            return;
-        }
-    }
+    let (tx, rx) = channel();
+    watch_config_for_changes(config_path.clone(), tx);
 
     unsafe {
         let workspace: id = msg_send![class!(NSWorkspace), sharedWorkspace];
         let notification_center: id = msg_send![workspace, notificationCenter];
 
-        let block = ConcreteBlock::new(|notification: id| {
-            let app: id = msg_send![notification, userInfo];
-            let running_app: id = msg_send![app, objectForKey: NSString::alloc(nil).init_str("NSWorkspaceApplicationKey")];
-            let app_name: id = msg_send![running_app, localizedName];
-            let app_name_str: *const std::os::raw::c_char = msg_send![app_name, UTF8String];
+        let block = ConcreteBlock::new({
+            let config = Arc::clone(&config);
+            move |notification: id| {
+                let app: id = msg_send![notification, userInfo];
+                let running_app: id = msg_send![app, objectForKey: NSString::alloc(nil).init_str("NSWorkspaceApplicationKey")];
+                let app_name: id = msg_send![running_app, localizedName];
+                let app_name_str: *const std::os::raw::c_char = msg_send![app_name, UTF8String];
 
-            let name = std::ffi::CStr::from_ptr(app_name_str)
-                .to_string_lossy()
-                .into_owned();
-
-            #[cfg(debug_assertions)]
-            {
-                println!("App activated: {}", name);
-            }
-
-            if is_terminal(&name) {
-                let current_input_source = get_current_input_source();
-                if !current_input_source.contains("keylayout.ABC") {
-                    #[cfg(debug_assertions)]
-                    {
-                        println!("Switching to ABC input");
-                    }
-                    switch_input_source("com.apple.keylayout.ABC");
+                let name = std::ffi::CStr::from_ptr(app_name_str)
+                    .to_string_lossy()
+                    .into_owned();
+                #[cfg(debug_assertions)]
+                {
+                    println!("Current app {}", name);
                 }
-            } else if is_chat_app(&name) {
-                let current_input_source = get_current_input_source();
-                if !current_input_source.contains("pinyin") {
-                    #[cfg(debug_assertions)]
-                    {
-                        println!("Switching to Pinyin input");
+
+                if let Ok(current_config) = config.read() {
+                    if let Some(input_source) = current_config.app.get(&name) {
+                        let current_input_source = get_current_input_source();
+                        if !current_input_source.contains(input_source) {
+                            #[cfg(debug_assertions)]
+                            {
+                                println!("Switching to input: {}", input_source);
+                            }
+                            switch_input_source(input_source);
+                        }
                     }
-                    switch_input_source("com.apple.inputmethod.SCIM.ITABC");
                 }
             }
         });
@@ -165,6 +222,21 @@ fn main() {
             usingBlock: block
         ];
 
-        CFRunLoopRun();
+        loop {
+            if let Ok(()) = rx.try_recv() {
+                let new_config = ensure_then_load(&config_path);
+                if let Ok(mut locked_config) = config.write() {
+                    *locked_config = new_config;
+                    println!("Config reloaded: {:?}", *locked_config);
+                }
+            }
+
+            // use `CFRunLoopRunInMode` avoid block
+            CFRunLoopRunInMode(
+                CFString::new(K_CF_RUN_LOOP_DEFAULT_MODE).as_concrete_TypeRef(),
+                0.1,
+                false as u8,
+            );
+        }
     }
 }
